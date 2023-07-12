c009ccb1fe8004fb18f2af7174f7b67ed273d341
socheatathai <socheatathai62@gmail.com>
2023-07-12 13:17:07 +0700

    first commit

---
+++ src/test/java/api/user/apiUser/SpringApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package api.user.apiUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
